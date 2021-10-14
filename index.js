eb6b08dc743c42092619c20a8766f410126e715d
Joaquin <joaquin050416@gmail.com>
2021-10-14 10:33:59 -0500

    Mover un objeto

--- index.js
+++ index.js
@@ -2,4 +2,15 @@ let elemento = document.getElementById('elemento');
 window.addEventListener('keydown', (e) =>{
   let tecla = e.key;
   console.log( e.key)
+
+  switch (tecla) {
+    case 'ArrowUp':
+      elemento.style.transform = "translateY(-100px)"
+      break;
+    case 'ArrowDown':
+      elemento.style.transform = "translateY(100px)"
+      break;
+    default:
+
+  }
 })
